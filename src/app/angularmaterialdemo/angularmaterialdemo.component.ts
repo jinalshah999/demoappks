3a28aadcfda1d59ed250e9da67cf5aefdfb18992
jinalshah999 <jinalshah999@gmail.com>
2020-11-24 09:45:38 +0530

    push changes

---
+++ src/app/angularmaterialdemo/angularmaterialdemo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-angularmaterialdemo',
+  templateUrl: './angularmaterialdemo.component.html',
+  styleUrls: ['./angularmaterialdemo.component.css']
+})
+export class AngularmaterialdemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { AngularmaterialdemoComponent } from './angularmaterialdemo/angularmaterialdemo.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, AngularmaterialdemoComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
